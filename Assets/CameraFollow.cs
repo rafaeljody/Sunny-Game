670ed9fd87a17fb5a4772ac3d82b8cc1384a6f62
Rafael Jody <rafaeljody@gmail.com>
2022-08-27 12:16:39 +0700

    sunny

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    //[Range(1, 10)]
+    //public float smoothFactor;
+    //public Vector3 minValues, maxValues;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+        //Follow();
+    }
+
+    //void Follow()
+    //{
+    //    // define min and max x,y,z values
+    //    Vector3 targetPosition = target.position + offset;
+
+    //    // verify if the target pos out of bound or not
+    //    // limit it to tge min and max values
+
+    //    Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+    //    transform.position = smoothPosition;
+
+    //}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
+    }
+
+    //func dedicated for physics (called a fixed amount of time per second)
+    private void FixedUpdate()
+    {
+        // move our character
+        // Time.fixedDeltaTime -> constant movement
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
